28ce1dc7833d40c31214544d62947ecea776af44
Raphi <71217268+DSPIRL@users.noreply.github.com>
2025-03-18 07:58:22 -0400

    Testing install

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ if [[ "$operatingSystem" == "Arch Linux" ]]; then
 
     # Install YAY
     if [[ "$chassis" == "laptop" ]]; then
-        read -rp 'Would you like to install kanata? (Y/N): ' varInstallKanata
+        read -rp 'Would you like to install kanata for custom keyboard layouts? (Y/N): ' varInstallKanata
         if [[ "$varInstallKanata" == "Y" || "$varInstallKanata" == "y" ]]; then
             sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
             cargo install kanata
@@ -19,13 +19,44 @@ if [[ "$operatingSystem" == "Arch Linux" ]]; then
         read -rp 'Would you like to install the yay AUR helper? (Y/N): ' varInstallYay
         if [[ "$varInstallYay" == "Y" || "$varInstallYay" == "y" ]]; then
             sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+            yay -S carapace-bin
         fi
+    fi
+
+    ##### USER CHOICES #####
+    read -rp 'Do you want to install TMUX? (Y/N): ' varInstallTmux
+    if [[ "$varInstallTmux" == "Y" || "$varInstallTmux" == "y" ]]; then
+        sudo pacman -S tmux
+    fi
+
+    read -rp 'Do you want to install DevTooling? (Y/N): ' varInstallDevtools
+    if [[ "$varInstallDevtools" == "Y" || "$varInstallDevtools" == "y" ]]; then
+        sudo pacman -S $(awk -v RS= '{$1=$1}1' archDevPackages.txt)
+    fi
+
+    read -rp 'Do you want to install Wireguard? (Y/N): ' varInstallWireguard
+    if [[ "$varInstallWireguard" == "Y" || "$varInstallWireguard" == "y" ]]; then
+        sudo pacman -S wireguard-tools
+    fi
+
+    read -rp 'Do you want to install and setup Syncthing? (Y/N): ' varInstallSyncthing
+    if [[ "$varInstallSyncthing" == "Y" || "$varInstallSyncthing" == "y" ]]; then
+        sudo pacman -S syncthing
+        systemctl --user enable syncthing.service
+        systemctl --user start syncthing.service
     fi
 
     read -rp 'Do you want to setup virtual machines? (Y/N): ' varArchVM
     if [[ "$varArchVM" == "Y" || "$varArchVM" == "y" ]]; then
         sudo pacman -S $(awk -v RS= '{$1=$1}1' archVMPackages.txt)
+        echo "Please review \"archVirtualizationInstruction.md\" to complete setup."
     fi
+
+    read -rp 'Run stow automatically? WARNING: This will overwrite conflicting already on your machine. (Y/N): ' varRunStow
+    if [[ "$varRunStow" == "Y" || "$varRunStow" == "y" ]]; then
+        rm -rf $(awk -v RS= '{$1=$1}1' .removeFiles.txt)
+    fi
+    ##### END USER CHOICES #####
 
 elif [[ "$operatingSystem" == "Darwin" ]]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
