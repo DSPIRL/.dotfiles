0cf4d069d5f56ce076277350deb70b6b7974bbf8
Raphi <71217268+DSPIRL@users.noreply.github.com>
2025-03-22 10:28:31 -0400

    another try

--- install.sh
+++ install.sh
@@ -34,60 +34,60 @@ WHEN YOU GET TO THE OH-MY-ZSH INSTALL AND IT ASKS IF YOU WANT TO SET ZSH AS THE 
     fi
 
     read -rp 'Would you like to install the yay AUR helper? (Y/N): ' varInstallYay
-    if [[ "$varInstallYay" == "Y" || "$varInstallYay" == "y" ]]; then
+    if [[ "${varInstallYay^^}" == "Y" ]]; then
         read -rp 'Do you want to install Carapace completers? (Y/N): ' varInstallCarapace
     fi
     read -rp 'Do you want to install TMUX? (Y/N): ' varInstallTmux
 
     sudo pacman -S $(awk -v RS= '{$1=$1}1' ~/.dotfiles/package_lists/archPackages.txt) $([[ ${varInstallDevtools^^} == "Y" ]] && awk -v RS= '{$1=$1}1' ~/.dotfiles/package_lists/archDevPackages.txt) $([[ "${varInstallSyncthing^^}" == "Y" ]] && echo syncthing) $([[ ${varInstallTmux^^} == "Y" ]] && echo tmux) $([[ ${varInstallWireguard^^} == "Y" ]] && echo wireguard-tools) $([[ ${varArchVM^^} == "Y" ]] && awk -v RS= '{$1=$1}1' ~/.dotfiles/package_lists/archVMPackages.txt) $([[ ${varInstallDeluge^^} == "Y" ]] && echo "deluge deluge-gtk") $([[ ${varInstallYay^^} == "Y" ]] && echo "go")
 
-    if [[ "$varInstallKanata" == "Y" || "$varInstallKanata" == "y" ]]; then
+    if [[ "${varInstallKanata^^}" == "Y" ]]; then
         curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
     fi
 
-    if [[ "$varInstallSyncthing" == "Y" || "$varInstallSyncthing" == "y" ]]; then
+    if [[ "${varInstallSyncthing^^}" == "Y" ]]; then
         systemctl --user enable syncthing.service
         systemctl --user start syncthing.service
     fi
 
-    if [[ "$varRunStow" == "Y" || "$varRunStow" == "y" ]]; then
+    if [[ "${varRunStow^^}" == "Y" ]]; then
         rm -rf ~/.config/alacritty ~/.config/ghostty ~/.config/hypr ~/.config/kanata ~/.config/nushell ~/.config/nvim ~/.config/starship.toml ~/.config/tmux ~/.config/wezterm ~/.ideavimrc ~/.profile ~/.vimrc ~/.zshenv ~/.zshrc ~/.tmux
         stow .
     fi
 
-    if [[ "$varInstallTmux" == "Y" || "$varInstallTmux" == "y" ]]; then
+    if [[ "${varInstallTmux^^}" == "Y" ]]; then
         git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
     fi
 
     # ohmyzsh
-    if [[ "$varInstallOMZ" == "Y" || "$varInstallOMZ" == "y" ]]; then
+    if [[ "${varInstallOMZ^^}" == "Y" ]]; then
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
         rm ~/.zshrc
     fi
 
-    if [[ "$varRunStow" == "Y" || "$varRunStow" == "y" ]]; then
+    if [[ "${varRunStow^^}" == "Y" ]]; then
         stow .
     fi
 
-    if [[ "$varInstallYay" == "Y" || "$varInstallYay" == "y" ]]; then
+    if [[ "${varInstallYay^^}" == "Y" ]]; then
         sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
     fi
 
-    if [[ "$varInstallCarapace" == "Y" || "$varInstallCarapace" == "y" ]]; then
+    if [[ "${varInstallCarapace^^}" == "Y" ]]; then
         yay -S carapace-bin
     fi
 
-    if [[ "$varInstallBraveBrowser" == "Y" || "$varInstallBraveBrowser" == "y" ]]; then
+    if [[ "${varInstallBraveBrowser^^}" == "Y" ]]; then
         flatpak install flathub com.brave.Browser
         sudo ln -fvs /var/lib/flatpak/exports/bin/com.brave.Browser /usr/bin/brave
     fi
 
-    if [[ "$varInstallVaria" == "Y" || "$varInstallVaria" == "y" ]]; then
+    if [[ "${varInstallVaria^^}" == "Y" ]]; then
         flatpak install flathub io.github.giantpinkrobots.varia
         sudo ln -fvs /var/lib/flatpak/exports/bin/io.github.giantpinkrobots.varia /usr/bin/varia
     fi
 
-    if [[ "$varInstallKanata" == "Y" || "$varInstallKanata" == "y" ]]; then
+    if [[ "${varInstallKanata^^}" == "Y" ]]; then
         $HOME/.cargo/bin/cargo install kanata
     fi
 
@@ -101,7 +101,7 @@ WHEN YOU GET TO THE OH-MY-ZSH INSTALL AND IT ASKS IF YOU WANT TO SET ZSH AS THE 
         chsh -s /usr/bin/nu
     fi
 
-    if [[ "$varArchVM" == "Y" || "$varArchVM" == "y" ]]; then
+    if [[ "${varArchVM^^}" == "Y" ]]; then
         echo "Please review \"archVirtualizationInstruction.md\" to complete setup."
     fi
 
@@ -110,7 +110,7 @@ elif [[ "$operatingSystem" == "Darwin" ]]; then
     brew install $(awk -v RS= '{$1=$1}1' ~/.dotfiles/package_lists/brewPackages.txt)
 else
     read -rp 'Are you on a Debian based disto? (Y/N): ' varDebian
-    if [[ "$varDebian" == "Y" || "$varDebian" == "y" ]]; then
+    if [[ "${varDebian^^}" == "Y" ]]; then
         sudo apt install $(awk -v RS= '{$1=$1}1' ~/.dotfiles/package_lists/debianPackages.txt)
         chsh -s /usr/bin/zsh
     fi
