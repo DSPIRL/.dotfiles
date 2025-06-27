390b9eff11571edd27e47f7e967797d8fcb2f3b4
raphi <71217268+DSPIRL@users.noreply.github.com>
2025-06-27 13:16:46 -0400

    Fixed a rofi bug

--- .config/hypr/scripts/rofi-clipboard.sh
+++ .config/hypr/scripts/rofi-clipboard.sh
@@ -4,5 +4,5 @@ if pgrep -x "rofi" >/dev/null; then
     pkill -x "rofi"
 else
     rofi_theme="$HOME/.config/rofi/config-clipboard.rasi"
-    rofi -modi clipboard:cliphist-rofi-img -config $rofi_theme -show clipboard -show-icons
+    rofi -modi clipboard:~/.local/bin/cliphist-rofi-img -config $rofi_theme -show clipboard -show-icons
 fi
