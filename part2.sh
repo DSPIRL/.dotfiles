ed6044e9fa045135c9f83ba8d2b335fa4c3c20f3
raphi <71217268+DSPIRL@users.noreply.github.com>
2025-07-07 20:43:48 -0400

    more script changes

--- part2.sh
+++ part2.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-# rm -rf ~/.config/nvim
-# rm -rf ~/.local/state/nvim
-# rm -rf ~/.local/share/nvim
+rm -rf ~/.config/nvim
+rm -rf ~/.local/state/nvim
+rm -rf ~/.local/share/nvim
 #
 # git clone https://github.com/LazyVim/starter ~/.config/nvim
-# rm -rf ~/.config/nvim/.git
+rm -rf ~/.config/nvim/.git
 #
-# cd $HOME
+cd $HOME
 
 paru -S carapace-bin
 
