c37af08d0b11b5f1dcf04e326f6de9e658742423
raphi <71217268+DSPIRL@users.noreply.github.com>
2025-07-08 17:44:20 -0400

    more script changes

--- part1.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-paru -S $(awk -v RS= '{$1=$1}1' ~/.dotfiles/package_lists/archPackages.txt) \
-    $(awk -v RS= '{$1=$1}1' ~/.dotfiles/package_lists/archHyprlandPackages.txt) \
-    $(awk -v RS= '{$1=$1}1' ~/.dotfiles/package_lists/archDevPackages.txt) \
-    $(echo "syncthing")
